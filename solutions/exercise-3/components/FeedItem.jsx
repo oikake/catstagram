59aa9b66eb7e191d9a5b076abf6f779637d1ef3a
johnco <johnny.p.cochrane@gmail.com>
2019-02-06 17:12:23 -0800

    made formating consistent

--- solutions/exercise-3/components/FeedItem.jsx
+++ solutions/exercise-3/components/FeedItem.jsx
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import Likes from './Likes.jsx';
+import React, { Component } from "react";
+import Likes from "./Likes.jsx";
 
 class FeedItem extends Component {
-    render() {
-        const { photo } = this.props;
-        const { url, likes } = photo;
-        return (
-            <div className="FeedItem">
-                <img className="Image" src={url} />
-                <Likes totalLikes={likes} />
-            </div>
-        );
-    }
+  render() {
+    const { photo } = this.props;
+    const { url, likes } = photo;
+    return (
+      <div className="FeedItem">
+        <img className="Image" src={url} />
+        <Likes totalLikes={likes} />
+      </div>
+    );
+  }
 }
 
 export default FeedItem;
-
